c4c6f29888401dcf645ddf9853e506933c2330d8
znurmanova@dar.llp <zukhra.nurmanova@gmail.com>
2025-09-18 17:33:19 +0500

    6 lesson and homework (2nd part)

--- src/test/java/ui/lesson_6_and_homework.java
+++ src/test/java/ui/lesson_6_and_homework.java
@@ -1,12 +1,14 @@
 package ui;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class lesson_6_and_homework {
 
@@ -31,10 +33,22 @@ public class lesson_6_and_homework {
         Thread.sleep(1000);
         webFormButton.click();
 
-        driver.findElement(By.xpath("//form")).submit();
-        Thread.sleep(3000);
+        //driver.findElement(By.xpath("//form")).submit();
+        //Thread.sleep(3000);
+        //driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-        driver.findElement(By.xpath("//button[@type='submit']"));
+        WebElement selectElement = driver.findElement(By.name("my-select"));
+        Select select = new Select(selectElement);
+        String actualText = select.getFirstSelectedOption().getText();
+        Assertions.assertEquals("Open this select menu", actualText);
+
+        select.selectByIndex(2);
+        Assertions.assertEquals("Two", select.getFirstSelectedOption().getText());
+
+        select.selectByValue("3");
+        Assertions.assertEquals("Three", select.getFirstSelectedOption().getText());
+
+
     }
 
 }
