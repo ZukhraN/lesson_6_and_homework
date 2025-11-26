5efe43881a5323c87d4d1ff79b078af12e0ae887
znurmanova@dar.llp <zukhra.nurmanova@gmail.com>
2025-11-26 15:47:14 +0500

    Changes for CI CD - 2 part

--- src/test/java/ui/BasePageTests.java
+++ src/test/java/ui/BasePageTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -11,6 +12,18 @@ public class BasePageTests {
 
     protected static final String BASE_URL="https://bonigarcia.dev/selenium-webdriver-java/";
     WebDriver driver;
+
+    @BeforeEach
+    public void setUp1() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+    }
 
     @BeforeEach
     void setUp(){
